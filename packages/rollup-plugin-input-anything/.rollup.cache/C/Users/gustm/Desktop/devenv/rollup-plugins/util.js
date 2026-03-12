4982e8f2546819a86018f043dea67a227d32eb60
hsl0 <46296273+hsl0@users.noreply.github.com>
2026-03-13 03:40:37 +0900

    chore: remove unused cached files and update .gitignore

--- packages/rollup-plugin-input-anything/.rollup.cache/C/Users/gustm/Desktop/devenv/rollup-plugins/util.js
+++
@@ -1,9 +0,0 @@
-export function toArray(arr) {
-    return Array.isArray(arr) ? arr : arr ? [arr] : [];
-}
-export function extendIntroOutro(...args) {
-    return async (chunk) => (await Promise.all(args.map((x) => (typeof x === 'function' ? x(chunk) : x))))
-        .filter((x) => x)
-        .join('\n');
-}
-//# sourceMappingURL=util.js.map
